f3ffd65767599d2e95b9aeb1ebb6b47cac452f94
hdlam <hongdang@stud.ntnu.no>
2015-03-03 15:18:03 +0100

    moved the boids behaviour to a separate class

--- chirpBoid/src/sketch.ino
+++ chirpBoid/src/sketch.ino
@@ -1,7 +1,39 @@
 #include <Chirp.h>
+#include <Boids.h>
+
+Boids other[3];
+Boids me(20,15, PI);
+
+void debug(Boids b){
+}
+
+void setup(){
+    Serial.begin(9600);
+    other[0].setPos(50,50);
+    other[0].setAngle(PI);
+    other[0].setVel(10,20);
+
+    other[1].setPos(30,10);
+    other[1].setAngle((float)PI/3.0);
+    other[1].setVel(14,40);
+
+    other[2].setPos(5,50);
+    other[2].setAngle((float)3*PI/2);
+    other[2].setVel(15,30);
+}
+
+
+
+void loop(){
+    me.update(other);
+}
+
+
+
+/*#include <Chirp.h>
 #include <Vector2D.h>
 
-Vector2D pos; //needed?
+Vector2D pos;
 Vector2D vel;
 Vector2D acc;
 
@@ -9,9 +41,13 @@ float angle;
 float maxForce;
 float maxSpeed;
 
+float sepDist = 25;
+float aliDist = 50;
+float cohDist = 100;
+
 
 Vector2D& steerTo(Vector2D& target){
-    Vector2D des = target;
+    Vector2D des(target.x,target.y); //makes sure that we get a copy
     des.sub(pos);
     float d = des.length();
     if(d> 0){
@@ -23,22 +59,67 @@ Vector2D& steerTo(Vector2D& target){
     Vector2D temp;
     return temp;
 }
+Vector2D other(0,0);
 Vector2D& cohesion(){
     Vector2D coh(0,0);
     byte count = 0;
-    //do cohesion
-    return coh;
+    //get data
+    for(byte i = 0; i < 4; i++){
+        if(true){
+            coh.add(other);
+            count++;
+        }
+
+    }
+    if(count > 0){
+        coh.div(count);
+    }
+    else
+        return coh;
+    return steerTo(coh);
 }
 
 Vector2D& alignment(){
     Vector2D ali(0,0);
     byte count = 0;
     //do alignment
+    for(byte i = 0; i < 4; i++){
+        float dist = pos.dist(other);
+        if(dist < aliDist){
+            ali.add(other.vel);
+            count++;
+        }
+    }
+    if(count > 0)
+        ali.div(count);
+    
+    ali.normalize();
+    avg.mul(maxForce);
+
     return ali;
 }
 
 Vector2D& separation(){
     Vector2D sep(0,0);
+    byte count = 0;
+    for(byte i = 0; i < 4; i++){
+        float dist = pos.dist(other);
+        if(dist < sepDist){
+            Vector2D diff(pos.x, pos.y);
+            diff.sub(other.pos);
+            diff.normalize();
+            diff.div(dist);
+            sep.add(diff);
+            count++;
+        }
+    } 
+    if(count > 0)
+        sep.div(count);
+    if(sep.length()){
+        sep.normalize();
+        sep.mul(60);
+        sep.sub(vel);
+    }
     return sep;
 }
 
@@ -83,4 +164,4 @@ void loop()
 
 }
 
-
+*/
